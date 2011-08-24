87daafd1124d733bb7f02bb0692e63d25ace7fb6
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-08-24 21:49:14 +0200

    Added the get_favicon method, so we can get that icon. :)

--- getthatfavicon.py
+++ getthatfavicon.py
@@ -4,10 +4,20 @@
 A favicon downloader
 """
 
-class FaviconDownloader():
-    def __init__(self):
-        pass
+import urllib
+import sys
 
+class FaviconDownloader():
+    def __init__(self, url):
+        self.url = url
+        self.favicon = None
+        
+    def get_favicon(self):
+        filename = sys.path[0] + '//favicon.ico'
+        (self.favicon, headers) = urllib.urlretrieve(self.url, filename)
+        print(self.favicon, headers)
+        
 if __name__ == '__main__':
-    fd = FaviconDownloader()
+    fd = FaviconDownloader('http://python.org/favicon.ico')
     print(fd)
+    fd.get_favicon()
