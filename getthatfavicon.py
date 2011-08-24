cfa1a9b2cca5e8c745321f8c89b1b1db206f38a2
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-08-24 21:34:28 +0200

    Writed some skeleton.

---
+++ getthatfavicon.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+A favicon downloader
+"""
+
+class FaviconDownloader():
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    fd = FaviconDownloader()
+    print(fd)
