9c8ba0f9eaf23df7dab5ab58e55b686fd80e375d
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-08-25 09:21:58 +0200

    Added "try until we find it" feature. When the downloaded favicon isn't really an icon, we try to find it an upper dns level.

--- getthatfavicon.py
+++ getthatfavicon.py
@@ -7,33 +7,40 @@ A favicon downloader
 import urllib
 import urlparse
 import sys
+import os
 
 import Image
 
 class FaviconDownloader():
     def __init__(self, url):
         self.url = url
-        up = urlparse.urlparse(self.url)
-        self.favicon_url = up.scheme + '://' + up.netloc + '/favicon.ico'
-        self.filename = sys.path[0] + '//favicon.' + up.netloc + '.ico'
+        self.up = urlparse.urlparse(self.url)
+        self.favicon_url = self.up.scheme + '://' + self.up.netloc + '/favicon.ico'
+        self.filename = sys.path[0] + '//favicon.' + self.up.netloc + '.ico'
         self.favicon = None
     
-    def get_favicon_type(self):
+    def is_valid_favicon(self):
         try:
             img = Image.open(self.filename)
         except IOError as error:
             if error[0] == "cannot identify image file":
-                return 'NO_IMAGE'
-        return img.format
-        
+                return False
+        if img.format == 'ICO' or img.format == 'PNG':
+            return True
+        else:
+            return False
         
     def get_favicon(self):
         print(self.favicon_url)
         (self.favicon, headers) = urllib.urlretrieve(self.favicon_url, self.filename)
         print(self.favicon, headers)
-        if self.get_favicon_type() != 'ICO':
-            print(self.filename + " isn't an icon file.")
-        
+        if not self.is_valid_favicon():
+            print(self.filename + " isn't a valid favicon.")
+            os.remove(self.filename)
+            new_url = self.up.scheme + '://' + self.up.netloc[((self.up.netloc.find('.'))+1):]
+            new_fd = FaviconDownloader(new_url)
+            print(fd)
+            new_fd.get_favicon()        
         
 if __name__ == '__main__':
     fd = FaviconDownloader('http://docs.python.org')
