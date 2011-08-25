8f86a4a6cdbd69d771ecd1c07efa9e28b976f3e0
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-08-25 09:02:32 +0200

    Added is_valid_favicon function.

--- getthatfavicon.py
+++ getthatfavicon.py
@@ -8,21 +8,35 @@ import urllib
 import urlparse
 import sys
 
+import Image
+
 class FaviconDownloader():
     def __init__(self, url):
         self.url = url
         up = urlparse.urlparse(self.url)
-        self.favicon_url = up.scheme + '://' + self.netloc + '/favicon.ico'
+        self.favicon_url = up.scheme + '://' + up.netloc + '/favicon.ico'
         self.filename = sys.path[0] + '//favicon.' + up.netloc + '.ico'
         self.favicon = None
+    
+    def get_favicon_type(self):
+        try:
+            img = Image.open(self.filename)
+        except IOError as error:
+            if error[0] == "cannot identify image file":
+                return 'NO_IMAGE'
+        return img.format
+        
         
     def get_favicon(self):
         print(self.favicon_url)
-        (self.favicon, headers) = urllib.urlretrieve(self.favicon_url, filename)
+        (self.favicon, headers) = urllib.urlretrieve(self.favicon_url, self.filename)
         print(self.favicon, headers)
+        if self.get_favicon_type() != 'ICO':
+            print(self.filename + " isn't an icon file.")
+        
         
 if __name__ == '__main__':
-    fd = FaviconDownloader('http://python.org')
+    fd = FaviconDownloader('http://docs.python.org')
     print(fd)
     fd.get_favicon()
     fd = FaviconDownloader('http://www.google.com')
