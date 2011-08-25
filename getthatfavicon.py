c0a875c679609b59ffda553aa00c8daed78df4c1
Gyorgy Nadudvari <reedcourty@gmail.com>
2011-08-25 09:53:07 +0200

    Added capabilities of handle some malformed url.

--- getthatfavicon.py
+++ getthatfavicon.py
@@ -14,9 +14,17 @@ import Image
 class FaviconDownloader():
     def __init__(self, url):
         self.url = url
-        self.up = urlparse.urlparse(self.url)
-        self.favicon_url = self.up.scheme + '://' + self.up.netloc + '/favicon.ico'
-        self.filename = sys.path[0] + '//favicon.' + self.up.netloc + '.ico'
+        up = urlparse.urlparse(self.url)
+        if (up.scheme == ''):
+            self.scheme = 'http'
+        else:
+            self.scheme = up.scheme
+        if (up.netloc == ''):
+            self.netloc = up.path
+        else:
+            self.netloc = up.netloc
+        self.favicon_url = self.scheme + '://' + self.netloc + '/favicon.ico'
+        self.filename = sys.path[0] + '//favicon.' + self.netloc + '.ico'
         self.favicon = None
     
     def is_valid_favicon(self):
@@ -31,27 +39,27 @@ class FaviconDownloader():
             return False
         
     def get_favicon(self):
-        print(self.favicon_url)
+        print("\n" + self.favicon_url)
         (self.favicon, headers) = urllib.urlretrieve(self.favicon_url, self.filename)
-        print(self.favicon, headers)
+        print(self.favicon)
         if not self.is_valid_favicon():
             print(self.filename + " isn't a valid favicon.")
             os.remove(self.filename)
-            new_url = self.up.scheme + '://' + self.up.netloc[((self.up.netloc.find('.'))+1):]
+            new_url = self.scheme + '://' + self.netloc[((self.netloc.find('.'))+1):]
             new_fd = FaviconDownloader(new_url)
-            print(fd)
-            new_fd.get_favicon()        
+            new_fd.get_favicon()
         
 if __name__ == '__main__':
     fd = FaviconDownloader('http://docs.python.org')
-    print(fd)
     fd.get_favicon()
     fd = FaviconDownloader('http://www.google.com')
-    print(fd)
     fd.get_favicon()
     fd = FaviconDownloader('https://github.com/reedcourty/getthatfavicon')
-    print(fd)
     fd.get_favicon()
     fd = FaviconDownloader('http://www.doodle.com/73hqnfzzkbmnqeqa')
-    print(fd)
     fd.get_favicon()
+    fd = FaviconDownloader('http://translate.google.com/?hl=en#en|hu|added%20%22try%20until%20we%20find%20it%22%20feature.%20When%20the%20downloaded%20favicon%20isn%27t%20really%20an%20icon%20we%20try%20to%20find%20it%20an%20upper%20dns%20level.')
+    fd.get_favicon()
+    fd = FaviconDownloader('ratucen.sch.bme.hu')
+    fd.get_favicon()
+    
